343a920d6eb8c0fe323bbd87a690716e8431d62e
Broshen <boshen.cui@gmail.com>
2016-07-25 21:50:06 -0400

    configured bootsy to upload images via ftp to website

--- Gemfile
+++ Gemfile
@@ -39,6 +39,9 @@ gem 'truncato'
 # environment variables
 gem 'figaro'
 
+# uploads storage
+gem 'carrierwave-ftp', :require => 'carrierwave/storage/ftp' # FTP only
+
 group :development, :test do
   # Use sqlite3 as the database for Active Record
   gem 'sqlite3'
--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,15 @@ module Website
         host: "boshencui.herokuapp.com"   
     }
 
+    CarrierWave.configure do |config|
+      config.ftp_host = "ftp.boshencui.com"
+      config.ftp_port = 21
+      config.ftp_user = "boshenftp@boshencui.com"
+      config.ftp_passwd = ENV["FTP_PASSWORD"]
+      config.ftp_folder = ""
+      config.ftp_url = "http://boshencui.com"
+      config.ftp_passive = false # false by default
+    end
+
   end
 end
