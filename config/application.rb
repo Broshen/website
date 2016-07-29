478cdc6b3d6a0ad5af5a4c70714ce1fd8b26eb13
Broshen <boshen.cui@gmail.com>
2016-07-28 20:40:47 -0400

    fml

--- config/application.rb
+++ config/application.rb
@@ -39,7 +39,7 @@ module Website
     }
 
     CarrierWave.configure do |config|
-    #  config.fog_provider = 'fog/aws'                        # required
+      config.fog_provider = 'fog/aws'                        # required
       config.fog_credentials = {
         provider:              'AWS',                        # required
         aws_access_key_id:     ENV["ACCESS_KEY"],                        # required
