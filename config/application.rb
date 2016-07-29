83f16869890a7cebcefa4d1a44a7973bc8365bc4
Broshen <boshen.cui@gmail.com>
2016-07-28 21:50:37 -0400

    fuck

--- config/application.rb
+++ config/application.rb
@@ -39,13 +39,15 @@ module Website
     }
 
     CarrierWave.configure do |config|
-      config.fog_credentials = {
-        provider:              'AWS',                        # required
-        aws_access_key_id:     ENV["ACCESS_KEY"],                        # required
-        aws_secret_access_key: ENV["SECRET_KEY"],                        # required
+        config.storage    = :aws
+        config.aws_bucket = 'bootsyuploads'
+        config.aws_acl    = 'public-read'
+
+      config.fog_credentials = {                     # required
+        access_key_id:     ENV["ACCESS_KEY"],                        # required
+        secret_access_key: ENV["SECRET_KEY"],                        # required
         region:                'us-east-1',                  # optional, defaults to 'us-east-1'
-        host:                  's3.amazonaws.com',             # optional, defaults to nil
-        endpoint:              'https://s3.amazonaws.com' # optional, defaults to nil
+ 
       }
       config.fog_directory  = 'bootsyuploads'                          # required
       config.fog_public     = false                                        # optional, defaults to true
