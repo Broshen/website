2760f2fec35f6b6020af91ef63251010712d4f86
Broshen <boshen.cui@gmail.com>
2016-07-28 21:22:20 -0400

    fuck

--- config/application.rb
+++ config/application.rb
@@ -45,9 +45,9 @@ module Website
         aws_secret_access_key: ENV["SECRET_KEY"],                        # required
         region:                'us-east-1',                  # optional, defaults to 'us-east-1'
         host:                  's3.amazonaws.com',             # optional, defaults to nil
-        endpoint:              'https://s3.amazonaws.com/bootsyuploads' # optional, defaults to nil
+        endpoint:              'https://s3.amazonaws.com' # optional, defaults to nil
       }
-      config.fog_directory  = ''                          # required
+      config.fog_directory  = 'bootsyuploads'                          # required
       config.fog_public     = false                                        # optional, defaults to true
  
     end
