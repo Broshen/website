694d8c7008e5eff46193479d1022d7b359bbd0e1
Broshen <boshen.cui@gmail.com>
2016-09-22 17:19:16 -0400

    added thumbnail to experiences

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160719014922) do
+ActiveRecord::Schema.define(version: 20160921231907) do
 
   create_table "bootsy_image_galleries", force: :cascade do |t|
     t.integer  "bootsy_resource_id"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20160719014922) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.text     "links"
+    t.text     "thumbnail"
   end
 
   create_table "projects", force: :cascade do |t|
